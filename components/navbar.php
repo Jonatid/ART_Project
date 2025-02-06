8bfdb84bb210d1767c5cb05bd440cce73dda3a7b
Jeanett Napier <aridethere1@gmail.com>
2025-02-05 19:53:49 -0500

    Add navbar.php

---
+++ components/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar">
+    <div class="logo">A Ride There</div>
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li class="dropdown">
+            <a href="#">Sign Up / Sign In</a>
+            <ul class="dropdown-content">
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="signin.php">Sign In</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
